1b9920898f03a9fa4888cf031dc7e53023c2dc04
crislopes3005 <148553965+crislopes3005@users.noreply.github.com>
2025-02-05 14:33:40 -0300

    segundo commit

--- dags/teste.py
+++ dags/teste.py
@@ -11,7 +11,7 @@ import pendulum
 
 @aql.dataframe(task_id="python_1")
 def python_1_func():
-    print('Teste 2')
+    print('Teste 2.1')
 
 default_args={
     "owner": "Cristiane Lopes,Open in Cloud IDE",
